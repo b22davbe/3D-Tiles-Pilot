57e5ffe4a1f123f26c6311357ef272017ad285f8
b22davbe <129262690+b22davbe@users.noreply.github.com>
2025-03-31 16:22:19 +0200

    Add enviroment for the 3D-Tiles pilot

--- app.js
+++ app.js
@@ -1,32 +1,69 @@
-// Starta timer direkt när sidan laddas
-const startTime = performance.now();
 // Grant CesiumJS access to your ion assets
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MWVmMWFiNi00YWM1LTQxZWUtOTFkYy0xM2M2MmUzYTZjMDIiLCJpZCI6MjgxMDQ1LCJpYXQiOjE3NDEwOTkyNjl9.Ji4X--LQ3PLpuJoBnA-aipNj642E9aSV3SWi-TIP86g";
 
-window.viewer = new Cesium.Viewer("cesiumContainer", {
+const viewer = new Cesium.Viewer("cesiumContainer", {
   terrainProvider: await Cesium.CesiumTerrainProvider.fromIonAssetId(1),
 });
 
 viewer.scene.globe.depthTestAgainstTerrain = true;
+window.viewer = viewer;
+
+// Fly to Skara Domkyrka
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(13.43450246, 58.38425682, 407.51),
+  orientation: {
+    heading: Cesium.Math.toRadians(46.79851),
+    pitch: Cesium.Math.toRadians(-33.11150),
+    roll: 0.00044,
+  },
+  duration: 2,
+});
 
 try {
-  const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(69380);
+  const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(96188); // OSM Buildings
   viewer.scene.primitives.add(tileset);
   await viewer.zoomTo(tileset);
 
-  const checkCesiumReady = setInterval(() => {
-    if (window.viewer.scene.frameState.frameNumber > 5) {
-      clearInterval(checkCesiumReady);
-
-      const renderTime = Math.round(performance.now() - startTime);
-      console.log(` Rendertime: ${renderTime} ms`);
-
-      // 🟢 Skicka eventet så att Userscriptet kan ta
-      window.dispatchEvent(new CustomEvent("cesiumRenderTime", { detail: renderTime }));
-    }
-  }, 100);
+  // Apply default style om det finns extra info
+  const extras = tileset.asset.extras;
+  if (
+    Cesium.defined(extras) &&
+    Cesium.defined(extras.ion) &&
+    Cesium.defined(extras.ion.defaultStyle)
+  ) {
+    tileset.style = new Cesium.Cesium3DTileStyle({
+      ...extras.ion.defaultStyle,
+      show: {
+        conditions: [
+          // ["${name} === 'Skara Domkyrka'", "false"], // Hides the church from OSM Buildings 
+          ["true", "true"],
+        ],
+      },
+    });
+  }
 } catch (error) {
   console.log(error);
 }
 
+// Debugger for setting cameraposition in the flyTo() function
+
+// viewer.camera.changed.addEventListener(() => {
+//   const cartesian = viewer.camera.position;
+//   const cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+
+//   if (!cartographic) return;
+
+//   const lon = Cesium.Math.toDegrees(cartographic.longitude).toFixed(8);
+//   const lat = Cesium.Math.toDegrees(cartographic.latitude).toFixed(8);
+//   const height = cartographic.height.toFixed(2);
+
+//   const heading = Cesium.Math.toDegrees(viewer.camera.heading).toFixed(5);
+//   const pitch = Cesium.Math.toDegrees(viewer.camera.pitch).toFixed(5);
+//   const roll = Cesium.Math.toDegrees(viewer.camera.roll).toFixed(5);
+
+//   console.clear();
+//   console.log(`cameraposition:`);
+//   console.log(`Lon: ${lon}, Lat: ${lat}, Height: ${height}`);
+//   console.log(`Heading: ${heading}, Pitch: ${pitch}, Roll: ${roll}`);
+// });
 
